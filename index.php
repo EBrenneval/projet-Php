b4c17c4eae42c6208a2748b7c9f024d5435b3a8a
Eric.A <arnhemeric60@gmail.com>
2017-05-26 13:50:00 +0200

    ajout de la navbar et du php qui permet d'afficher le bon contenu en fonction du bouton

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+    // Si le fichier source.xml existe
+    if (file_exists('source.xml')) {
+        // On le charge dans la variable $xml
+        $xml = simplexml_load_file('source.xml');
+    }
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -8,7 +15,40 @@
         <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     </head>
     <body>
-
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#buttons_navbar" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Projet PHP</a>
+                </div>
+                <div class="collapse navbar-collapse" id="buttons_navbar">
+                    <ul class="nav navbar-nav">
+                        <?php
+                            // Pour chaque page dans la tableau page du tableau xml
+                            foreach ($xml->page as $page) {?>
+                                <!-- On affiche un li avec les bonnes données -->
+                                <li><a href="index.php?p=<?= $page['id'] ?>"><?= $page->menu; ?></a></li><?php
+                            }
+                        ?>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <?php
+            // Si $_GET['p'] existe
+            if(isset($_GET['p'])){
+                // On stocke sa valeur dans la variable $page
+                // On retire 1 car l'id de sélection commence 0 et non à 1 (et transforme en entier)
+                $page = $_GET['p'] - 1;
+                // On affiche le bon contenu en sélectionnant avec la valeur de $page
+                echo $xml->page[$page]->content;
+            }
+        ?>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </body>
 </html>
