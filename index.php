9f80e8044d258ad7bca83ec6694abac12bac1bd6
EloB <elodie.brenneval@gmail.com>
2017-05-26 14:38:50 +0200

    ajout des fichier

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+if (file_exists('source.xml')) {  // on vérifie si le fichier existe
+    $xml = simplexml_load_file('source.xml'); //  ds la variable $xml, on charge le fichier xml
+}
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -9,6 +14,19 @@
     </head>
     <body>
 
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <ul>
+            <?php
+            foreach ($xml->page as $page) {  // boucle qui parcourt le fichier xml et recup chaque enfant ds le parent
+                ?><li><a href="<?= $page['id'] ?>.html"><?= $page->menu ?></a></li> <!--  on récup les id pr chaque lien.-->
+            <?php }
+            ?>
+        </ul>
+        <?php
+        if (isset($_GET['p'])) {// on vérifie que ca existe et que c'est correct. 
+            $page = $_GET['p'] - 1;  // on stocke le GET dans une variable. le -1 pour commencer a 1 sinon on commencerai à 0. 
+            echo $xml->page[$page]->content;  // on affiche le contenu . 
+        }
+        ?>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </body>
 </html>
