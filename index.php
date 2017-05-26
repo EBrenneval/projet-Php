1b431c0260500b7f889e926f5fa7009b4a8673f1
vero <vero.boulnois@gmail.com>
2017-05-26 14:40:19 +0200

    projet en cours

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+// si fichier existe avec la variable xml il charge le fichier
+if (file_exists('source.xml')) {
+    $xml = simplexml_load_file('source.xml');
+}
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -8,7 +14,19 @@
         <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     </head>
     <body>
-
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <ul>
+            <?php
+            foreach ($xml->page as $page) {  // boucle qui parcourt le fichier xml et recup chaque enfant ds le parent
+                ?><li><a href="<?= $page['id'] ?>.html"><?= $page->menu ?></a></li> <!--  on récup les id pr chaque lien.-->
+            <?php }
+            ?>
+        </ul>
+        <?php
+        if (isset($_GET['p'])) {// on vérifie que ca existe et que c'est correct.(p dans le li pour correspondre à page)
+            $page = $_GET['p'] - 1;  // on stocke le GET dans une variable - 1 pour index précédent
+            echo $xml->page[$page]->content;  // on affiche le contenu .
+        }
+        ?>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </body>
 </html>
