a150139496dcab527065bafbc3b9ce2ff4c97685
Eric.A <arnhemeric60@gmail.com>
2017-05-26 14:40:37 +0200

    ajout du .htaccess qui réécrit l'url + modif du titre en fonction du contenu

--- index.php
+++ index.php
@@ -1,15 +1,26 @@
 <?php
-    // Si le fichier source.xml existe
-    if (file_exists('source.xml')) {
-        // On le charge dans la variable $xml
-        $xml = simplexml_load_file('source.xml');
-    }
+// Si le fichier source.xml existe
+if (file_exists('source.xml')) {
+    // On le charge dans la variable $xml
+    $xml = simplexml_load_file('source.xml');
+}
 ?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Projet PHP</title>
+        <title>
+            <?php
+            // Si $_GET['p'] existe
+            if (isset($_GET['p'])) {
+                // On stocke sa valeur dans la variable $page
+                // On retire 1 car l'id de sélection commence 0 et non à 1 (et transforme en entier)
+                $page = $_GET['p'] - 1;
+                // On affiche le bon titre en sélectionnant avec la valeur de $page
+                echo $xml->page[$page]->title;
+            }
+            ?>
+        </title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <link rel="stylesheet" href="assets/css/style.css">
         <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
@@ -29,25 +40,26 @@
                 <div class="collapse navbar-collapse" id="buttons_navbar">
                     <ul class="nav navbar-nav">
                         <?php
-                            // Pour chaque page dans la tableau page du tableau xml
-                            foreach ($xml->page as $page) {?>
-                                <!-- On affiche un li avec les bonnes données -->
-                                <li><a href="index.php?p=<?= $page['id'] ?>"><?= $page->menu; ?></a></li><?php
-                            }
+                        // Pour chaque page dans la tableau page du tableau xml
+                        foreach ($xml->page as $page) {
+                            ?>
+                            <!-- On affiche un li avec les bonnes données -->
+                            <li><a href="<?= $page['id'] ?>.html"><?= $page->menu; ?></a></li><?php
+                        }
                         ?>
                     </ul>
                 </div>
             </div>
         </nav>
         <?php
-            // Si $_GET['p'] existe
-            if(isset($_GET['p'])){
-                // On stocke sa valeur dans la variable $page
-                // On retire 1 car l'id de sélection commence 0 et non à 1 (et transforme en entier)
-                $page = $_GET['p'] - 1;
-                // On affiche le bon contenu en sélectionnant avec la valeur de $page
-                echo $xml->page[$page]->content;
-            }
+        // Si $_GET['p'] existe
+        if (isset($_GET['p'])) {
+            // On stocke sa valeur dans la variable $page
+            // On retire 1 car l'id de sélection commence 0 et non à 1 (et transforme en entier)
+            $page = $_GET['p'] - 1;
+            // On affiche le bon contenu en sélectionnant avec la valeur de $page
+            echo $xml->page[$page]->content;
+        }
         ?>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </body>
